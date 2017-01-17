9b3b86765b9ec1938892e0ac03c7a442f8e4f936
jonas002 <jonas002@scdis-macbook-pro-2.local>
2017-01-17 03:00:50 -0500

    Upload research kit caret and alamofire.

--- TRU-Pain/AppDelegate.swift
+++ TRU-Pain/AppDelegate.swift
@@ -10,20 +10,37 @@
 import Foundation
 import UIKit
 import CoreData
-
+import UserNotifications
 
 
 @UIApplicationMain
-class AppDelegate: UIResponder, UIApplicationDelegate {
+class AppDelegate: UIResponder, UIApplicationDelegate, UNUserNotificationCenterDelegate {
 
     var window: UIWindow?
 
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
+        // Set up the style and color of the common UI elements
+        customizeUIStyle()
         
+        //MR Coredata Stack
         MagicalRecord.setupCoreDataStack(withAutoMigratingSqliteStoreNamed: ".TRU-Pain")
         
-        // Override point for customization after application launch.
+        
+        if #available(iOS 10.0, *) {
+            let authOptions : UNAuthorizationOptions = [.alert, .badge, .sound]
+            UNUserNotificationCenter.current().requestAuthorization(
+                options: authOptions,
+                completionHandler: {_,_ in })
+            
+            // For iOS 10 display notification (sent via APNS)
+            UNUserNotificationCenter.current().delegate = self
+            
+            // For iOS 10 data message (sent via FCM)
+//            FIRMessaging.messaging().remoteMessageDelegate = self
+            
+        }
+        
         return true
     }
 
@@ -46,9 +63,66 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     }
 
     func applicationWillTerminate(_ application: UIApplication) {
-        // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
+        self.saveContext()
+    }
+    
+    
+    // MARK: - Core Data stack
+    lazy var persistentContainer: NSPersistentContainer = {
+        /*
+         The persistent container for the application. This implementation
+         creates and returns a container, having loaded the store for the
+         application to it. This property is optional since there are legitimate
+         error conditions that could cause the creation of the store to fail.
+         */
+        let container = NSPersistentContainer(name: ".TRU-pain")
+        container.loadPersistentStores(completionHandler: { (storeDescription, error) in
+            if let error = error as NSError? {
+                // Replace this implementation with code to handle the error appropriately.
+                // fatalError() causes the application to generate a crash log and terminate. You should not use this function in a shipping application, although it may be useful during development.
+                
+                /*
+                 Typical reasons for an error here include:
+                 * The parent directory does not exist, cannot be created, or disallows writing.
+                 * The persistent store is not accessible, due to permissions or data protection when the device is locked.
+                 * The device is out of space.
+                 * The store could not be migrated to the current model version.
+                 Check the error message to determine what the actual problem was.
+                 */
+                fatalError("Unresolved error \(error), \(error.userInfo)")
+            }
+        })
+        return container
+    }()
+    
+    // MARK: - Core Data Saving support
+    
+    func saveContext () {
+        let context = persistentContainer.viewContext
+        if context.hasChanges {
+            do {
+                try context.save()
+            } catch {
+                // Replace this implementation with code to handle the error appropriately.
+                // fatalError() causes the application to generate a crash log and terminate. You should not use this function in a shipping application, although it may be useful during development.
+                let nserror = error as NSError
+                fatalError("Unresolved error \(nserror), \(nserror.userInfo)")
+            }
+        }
     }
 
 
+}
+extension AppDelegate {
+    func customizeUIStyle() {
+        
+        
+        //UI Color scheme
+        UINavigationBar.appearance().tintColor = Colors.careKitRed.color
+        UITabBar.appearance().tintColor = Colors.careKitRed.color
+        UITabBarItem.appearance().setTitleTextAttributes([NSForegroundColorAttributeName: Colors.careKitRed.color], for:.selected)
+        UITabBarItem.appearance().setTitleTextAttributes([NSForegroundColorAttributeName: Colors.careKitRed.color], for:.normal)
+        UITableViewCell.appearance().tintColor = Colors.careKitRed.color
+    }
 }
 
