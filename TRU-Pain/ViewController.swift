d3500a4baa9a28e3144e02832e5789e473866685
jonas002 <jonas002@scdis-macbook-pro-2.local>
2017-01-17 02:06:16 -0500

    Initial Commit

---
+++ TRU-Pain/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  TRU-Pain
+//
+//  Created by jonas002 on 1/17/17.
+//  Copyright © 2017 scdi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
